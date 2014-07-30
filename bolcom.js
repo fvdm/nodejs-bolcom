b7267155a5e221c776877208246e66c3fc8bc275
Franklin van de Meent <fr@nkl.in>
2014-07-31 01:13:49 +0200

    Added talk() function

--- bolcom.js
+++ bolcom.js
@@ -19,3 +19,107 @@ var app = {
   utils: {},
   account: {}
 }
+
+// Communicate
+function talk( cat, method, params, callback ) {
+  if( typeof params === 'function' ) {
+    var callback = params
+    var params = {}
+  }
+  params = params instanceof Object ? params : {}
+
+  // check api key
+  if( typeof api_key === 'string' && api_key.length > 0 ) {
+    params.apikey = api_key
+  } else {
+    doCallback( new Error('missing apikey') )
+    return
+  }
+
+  // prevent multiple callbacks
+  var complete = false
+  function doCallback( err, data ) {
+    if( !complete ) {
+      complete = true
+      callback( err, data || null )
+    }
+  }
+
+  // build request
+  params.format = 'json'
+  
+  var options = {
+    host: 'api.bol.com',
+    path: '/'+ cat +'/v4/'+ method +'?'+ querystring.stringify(params),
+    method: 'GET',
+    headers: {
+      'User-Agent': 'bolcom.js (https://frankl.in)'
+    }
+  }
+
+  var request = https.request(options)
+
+  // process response
+  request.on( 'response', function(response) {
+    var data = ''
+
+    response.on( 'data', function(ch) { data += ch })
+
+    response.on( 'close', function() {
+      doCallback(new Error('request dropped'))
+    })
+
+    response.on( 'end', function() {
+      var error = null
+      if( response.statusCode != 200 ) {
+        error = new Error('API error')
+      }
+
+      try {
+        data = JSON.parse( data )
+      } catch(e) {
+        error = new Error('invalid response')
+        error.err = e
+      }
+      
+      if( error ) {
+        error.code = response.statusCode
+        error.headers = response.headers
+        error.api = data instanceof Object ? data : {}
+        error.body = data instanceof Object ? null : data
+      }
+
+      doCallback( error, data )
+    })
+  })
+
+  // timeout
+  request.on( 'socket', function( socket ) {
+    if( api_timeout ) {
+      socket.setTimeout( api_timeout )
+      socket.on( 'timeout', function() {
+        request.abort()
+      })
+    }
+  })
+
+  // error
+  request.on( 'error', function( err ) {
+    if( err == 'ECONNRESET' ) {
+      var error = new Error('request timeout')
+    } else {
+      var error = new Error('request failed')
+    }
+    error.err = err
+    doCallback( error )
+  })
+  
+  // do it
+  request.end()
+}
+
+module.exports = function(apikey, timeout) {
+  api_key = apikey
+  api_timeout = timeout || api_timeout
+  return app
+}
