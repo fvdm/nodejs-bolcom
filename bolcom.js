3c9f997290ee080f61731df54a3b6bd84dddbe1c
Franklin van de Meent <fr@nkl.in>
2016-03-31 12:46:05 +0200

    Clean up code

--- bolcom.js
+++ bolcom.js
@@ -11,236 +11,433 @@ Service docs:     https://developers.bol.com
 */
 
 var http = require ('httpreq');
-var api_key = null;
-var api_timeout = 5000;
-
-app = {
-  catalog: {},
-  utils: {},
-  account: {}
+var settings = {
+  apikey: null,
+  timeout: 5000
 };
 
 
-app.catalog.search = function (props, callback) {
-  talk ('catalog', 'search', props, function (err, data) {
-    if (!err && data.products instanceof Array) {
-      for (var i = 0; i < data.products.length; i++) {
-        data.products [i] = cleanProduct (data.products [i]);
-      }
-    }
-    callback (err, data);
-  });
-};
+/**
+ * Clean up product data
+ *
+ * @param product {object} - Object to clean
+ * @returns product {object} - Cleaned object
+ */
 
-
-app.catalog.lists = function (props, callback) {
-  talk ('catalog', 'lists', props, function (err, data) {
-    if (!err && data.products instanceof Array) {
-      for (var i = 0; i < data.products.length; i++) {
-        data.products [i] = cleanProduct (data.products [i]);
-      }
-    }
-    callback (err, data);
-  });
-};
-
-
-app.catalog.products = function (productId, props, callback) {
-  if (typeof props === 'function') {
-    var callback = props;
-    var props = {};
-  }
-  talk ('catalog', 'products/'+ productId, props, function (err, data) {
-    if (!err && data.products instanceof Array) {
-      for (var i = 0; i < data.products.length; i++) {
-        data.products [i] = cleanProduct (data.products [i]);
-      }
-    }
-    callback (err, data);
-  });
-};
-
-
-app.catalog.offers = function (productId, props, callback) {
-  if (typeof props === 'function') {
-    var callback = props;
-    var props = {};
-  }
-  talk ('catalog', 'offers/'+ productId, props, function (err, data) {
-    if (!err && data.offerData) {
-      data = data.offerData;
-    }
-    callback (err, data);
-  });
-};
-
-
-app.catalog.recommendations = function (productId, props, callback) {
-  if (typeof props === 'function') {
-    var callback = props;
-    var props = {};
-  }
-  talk ('catalog', 'recommendations/'+ productId, props, function (err, data) {
-    if (!err && data.products) {
-      data = data.products;
-      if (data instanceof Array && data.length >= 1) {
-        for (var i = 0; i < data.length; i++) {
-          data [i] = cleanProduct (data [i]);
-        }
-      }
-    }
-    callback (err, data);
-  });
-};
-
-
-app.catalog.relatedproducts = function (productId, props, callback) {
-  if (typeof props === 'function') {
-    var callback = props;
-    var props = {};
-  }
-  talk ('catalog', 'relatedproducts/'+ productId, props, function (err, data) {
-    if (!err && data.productFamilies) {
-      data = data.productFamilies;
-      var tmp = {};
-      if (data instanceof Array && data.length >= 1 && data [0] .key) {
-        for (var i = 0; i < data.length; i++) {
-          tmp [data [i] .key] = data [i];
-          if (data [i] .productFamilyMembers instanceof Array) {
-            var tmp2 = {};
-            for (var m = 0; m < data [i] .productFamilyMembers.length; m++) {
-              tmp2 [data [i] .productFamilyMembers [m] .label] = data [i] .productFamilyMembers [m];
-            }
-            tmp [data [i] .key] .productFamilyMembers = tmp2;
-          }
-        }
-        data = tmp;
-      }
-    }
-    callback (err, data);
-  });
-};
-
-
-app.utils.ping = function (callback) {
-  talk ('utils', 'ping', callback);
-};
-
-app.account.sessions = function (callback) {
-  talk ('accounts', 'sessions', callback);
-};
-
-
-// Clean up product
 function cleanProduct (product) {
+  var purls = {};
+  var pimgs = {};
+  var groups = {};
+  var group;
+  var image;
+  var attrib;
+  var url;
+  var i;
+  var a;
+
   // urls
   try {
-    var purls = {};
-    for (var i = 0; u < product.urls.length; u++) {
-      var url = product.urls [i];
+    for (i = 0; i < product.urls.length; i++) {
+      url = product.urls [i];
       purls [url.key] = url;
     }
     product.urls = purls;
-  } catch (e) {}
+  } catch (e) {
+    // skip
+  }
 
   // images
   try {
-    var pimgs = {};
-    for (var i = 0; m < product.images.length; m++) {
-      var image = product.images [i];
+    for (i = 0; i < product.images.length; i++) {
+      image = product.images [i];
       pimgs [image.key] = image;
     }
+
     product.images = pimgs;
-  } catch (e) {}
+  } catch (e) {
+    // skip
+  }
 
   // includeattributes: true
   try {
     if (product.attributeGroups) {
-      var groups = {};
-      for (var i = 0; g < product.attributeGroups.length; g++) {
-        var group = product.attributeGroups [i];
-        groups [group.title] = {title: group.title};
+      for (i = 0; i < product.attributeGroups.length; i++) {
+        group = product.attributeGroups [i];
+        groups [group.title] = {
+          title: group.title
+        };
 
         if (group.attributes) {
           groups [group.title] .attributes = {};
-          for (var a = 0; a < group.attributes.length; a++) {
-            var attrib = group.attributes [a];
+
+          for (a = 0; a < group.attributes.length; a++) {
+            attrib = group.attributes [a];
             groups [group.title] .attributes [attrib.key] = attrib;
           }
         }
       }
+
       product.attributeGroups = groups;
     }
-  } catch (e) {}
+  } catch (e) {
+    // skip
+  }
 
-  return product
+  return product;
 }
 
 
-// Communicate
-function talk (cat, method, params, callback) {
-  if (typeof params === 'function') {
-    var callback = params;
-    var params = {};
+/**
+ * Process HTTP response
+ *
+ * @callback callback
+ * @param err {Error,null} - Error instance
+ * @param res {object} - Response object
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function processResponse (err, res, callback) {
+  var data = '';
+  var error = null;
+
+  res = res || {
+    body: '',
+    statusCode: null
+  };
+
+  if (err) {
+    error = new Error ('request failed');
+    error.err = err;
+    callback (error);
+    return;
   }
-  params = params instanceof Object ? params : {};
+
+  if (res.statusCode >= 300) {
+    error = new Error ('API error');
+  }
+
+  try {
+    data = JSON.parse (res.body);
+  } catch (e) {
+    error = new Error ('invalid response');
+    error.err = e;
+  }
+
+  if (error) {
+    error.code = res.statusCode;
+    error.api = data instanceof Object ? data : {};
+    error.body = data instanceof Object ? null : res.body;
+  }
+
+  callback (error, data);
+}
+
+
+/**
+ * Communication with API
+ *
+ * @callback callback
+ * @param cat {string} - api.bol.com/:CAT/v4/method
+ * @param method {string} - api.bol.com/cat/v4/:METHOD
+ * @param [params] {object} - Request paramaters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function talk (cat, method, params, callback) {
+  var options = {
+    url: 'https://api.bol.com/' + cat + '/v4/' + method,
+    method: 'GET',
+    timeout: settings.timeout,
+    headers: {
+      'User-Agent': 'bolcom.js (https://www.npmjs.com/package/bolcom)'
+    }
+  };
+
+  // params is optional
+  if (typeof params === 'function') {
+    callback = params;
+    params = {};
+  }
 
   // check api key
-  if (typeof api_key === 'string' && api_key.length > 0) {
-    params.apikey = api_key;
-  } else {
+  if (!settings.apikey) {
     callback (new Error ('missing apikey'));
     return;
   }
 
-  // build request
+  params = params instanceof Object ? params : {};
+  params.apikey = settings.apikey;
   params.format = 'json';
-
-  var url = 'https://api.bol.com/'+ cat +'/v4/'+ method;
-  var options = {
-    parameters: params,
-    timeout: parseInt (api_timeout, 10),
-    headers: {
-      'User-Agent': 'npmjs.com/package/bolcom'
-    }
-  };
+  options.parameters = params;
 
   // process response
-  http.get (url, options, function (err, res) {
-    var data = res && res.body || null;
-    var error = null;
-
-    if (err) {
-      error = new Error ('request failed');
-      error.err = err;
-      callback (error);
-      return;
-    }
-
-    if (res.statusCode !== 200) {
-      error = new Error ('API error');
-    }
-
-    try {
-      data = JSON.parse (data);
-    } catch (e) {
-      error = new Error ('invalid response');
-      error.err = e;
-    }
-
-    if (error) {
-      error.code = res && res.statusCode || null;
-      error.headers = res && res.headers || null;
-      error.api = data instanceof Object ? data : {};
-      error.body = data instanceof Object ? null : data;
-    }
-
-    callback (error, data);
+  http.doRequest (options, function (err, res) {
+    processResponse (err, res, callback);
   });
 }
 
+
+/**
+ * Method: catalog.search
+ *
+ * @callback callback
+ * @param props {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogSearch (props, callback) {
+  talk ('catalog', 'search', props, function (err, data) {
+    var i;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.products instanceof Array) {
+      for (i = 0; i < data.products.length; i++) {
+        data.products [i] = cleanProduct (data.products [i]);
+      }
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: catalog.lists
+ *
+ * @callback callback
+ * @param props {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogLists (props, callback) {
+  talk ('catalog', 'lists', props, function (err, data) {
+    var i;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.products instanceof Array) {
+      for (i = 0; i < data.products.length; i++) {
+        data.products [i] = cleanProduct (data.products [i]);
+      }
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: catalog.products
+ *
+ * @callback callback
+ * @param productId {string} - Product ID
+ * @param [props] {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogProducts (productId, props, callback) {
+  if (typeof props === 'function') {
+    callback = props;
+    props = {};
+  }
+
+  talk ('catalog', 'products/' + productId, props, function (err, data) {
+    var i;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.products instanceof Array) {
+      for (i = 0; i < data.products.length; i++) {
+        data.products [i] = cleanProduct (data.products [i]);
+      }
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: catalog.offers
+ *
+ * @callback callback
+ * @param productId {string} - Product ID
+ * @param [props] {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogOffers (productId, props, callback) {
+  if (typeof props === 'function') {
+    callback = props;
+    props = {};
+  }
+
+  talk ('catalog', 'offers/' + productId, props, function (err, data) {
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.offerData) {
+      data = data.offerData;
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: catalog.recommendations
+ *
+ * @callback callback
+ * @param productId {string} - Product ID
+ * @param [props] {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogRecommendations (productId, props, callback) {
+  if (typeof props === 'function') {
+    callback = props;
+    props = {};
+  }
+
+  talk ('catalog', 'recommendations/' + productId, props, function (err, data) {
+    var i;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.products) {
+      data = data.products;
+
+      if (data instanceof Array && data.length >= 1) {
+        for (i = 0; i < data.length; i++) {
+          data [i] = cleanProduct (data [i]);
+        }
+      }
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: catalog.relatedproducts
+ *
+ * @callback callback
+ * @param productId {string} - Product ID
+ * @param [props] {object} - Method parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodCatalogRelatedProducts (productId, props, callback) {
+  if (typeof props === 'function') {
+    callback = props;
+    props = {};
+  }
+
+  talk ('catalog', 'relatedproducts/' + productId, props, function (err, data) {
+    var tmp = {};
+    var tmp2 = {};
+    var i;
+    var m;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+
+    if (data.productFamilies) {
+      data = data.productFamilies;
+
+      if (data instanceof Array && data.length >= 1 && data [0] .key) {
+        for (i = 0; i < data.length; i++) {
+          tmp [data [i] .key] = data [i];
+
+          if (data [i] .productFamilyMembers instanceof Array) {
+            tmp2 = {};
+
+            for (m = 0; m < data [i] .productFamilyMembers.length; m++) {
+              tmp2 [data [i] .productFamilyMembers [m] .label] = data [i] .productFamilyMembers [m];
+            }
+
+            tmp [data [i] .key] .productFamilyMembers = tmp2;
+          }
+        }
+
+        data = tmp;
+      }
+    }
+
+    callback (err, data);
+  });
+}
+
+
+/**
+ * Method: utils.ping
+ *
+ * @callback callback
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodUtilsPing (callback) {
+  talk ('utils', 'ping', callback);
+}
+
+
+/**
+ * Method: account.sessions
+ *
+ * @callback callback
+ * @returns {void}
+ */
+
+function methodAccountSessions (callback) {
+  talk ('accounts', 'sessions', callback);
+}
+
 module.exports = function (apikey, timeout) {
-  api_key = apikey;
-  api_timeout = timeout || api_timeout;
-  return app;
+  settings.apikey = apikey;
+  settings.timeout = timeout || settings.timeout;
+
+  return {
+    catalog: {
+      search: methodCatalogSearch,
+      lists: methodCatalogLists,
+      products: methodCatalogProducts,
+      offers: methodCatalogOffers,
+      recommendations: methodCatalogRecommendations,
+      relatedproducts: methodCatalogRelatedProducts
+    },
+    utils: {
+      ping: methodUtilsPing
+    },
+    account: {
+      sessions: methodAccountSessions
+    }
+  };
 };
